9d1a60879cf635bf67f6fae9fb71bde1ac4f2184
DanielFryy <dgz1606@gmail.com>
2020-02-03 00:49:50 -0500

    Addition of images and strings for the walkthrough

--- App/assets/images/index.js
+++ App/assets/images/index.js
@@ -1,5 +1,23 @@
 import logo from './Global/logo.png';
+import walkthrough1 from './Walkthrough/Page1/walkthrough1.png';
+import walkthroughRightArrow1 from './Walkthrough/Page1/walkthroughRightArrow1.png';
+import walkthroughPageIndicator1 from './Walkthrough/Page1/walkthroughPageIndicator1.png';
+import walkthrough2 from './Walkthrough/Page2/walkthrough2.png';
+import walkthroughRightArrow2 from './Walkthrough/Page2/walkthroughRightArrow2.png';
+import walkthroughPageIndicator2 from './Walkthrough/Page2/walkthroughPageIndicator2.png';
+import walkthrough3 from './Walkthrough/Page3/walkthrough3.png';
+import walkthroughRightArrow3 from './Walkthrough/Page3/walkthroughRightArrow3.png';
+import walkthroughPageIndicator3 from './Walkthrough/Page3/walkthroughPageIndicator3.png';
 
 export default {
   logo,
+  walkthrough1,
+  walkthroughRightArrow1,
+  walkthroughPageIndicator1,
+  walkthrough2,
+  walkthroughRightArrow2,
+  walkthroughPageIndicator2,
+  walkthrough3,
+  walkthroughRightArrow3,
+  walkthroughPageIndicator3,
 };
