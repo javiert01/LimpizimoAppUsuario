a1183ed175340c9e9c359de94c3c40a2637d00ef
DanielFryy <dgz1606@gmail.com>
2020-02-06 02:16:27 -0500

    Deletion of all unnecessary stuff

--- App/screens/Home/styles.js
+++ App/screens/Home/styles.js
@@ -2,44 +2,6 @@ import { StyleSheet } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 export default EStyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   //height: 1900,
-  //   paddingTop: 5,
-  //   backgroundColor: '#672D91',
-  //   alignItems: 'center',
-  //   justifyContent: 'flex-start',
-  // },
-  // map: {
-  //   ...StyleSheet.absoluteFillObject,
-  // },
-  // contentContainer: {
-  //   alignItems: 'flex-start',
-  //   width: '100%',
-  //   flex: 1,
-  //   paddingTop: 5,
-  //   backgroundColor: 'white',
-  // },
-  // formContainer: {
-  //   width: 350,
-  //   //flex: 1,
-  //   height: 500,
-  //   paddingTop: 5,
-  //   backgroundColor: 'white',
-  //   alignItems: 'center',
-  //   justifyContent: 'flex-start',
-  // },
-  // pickerContainer: {
-  //   alignItems: 'center',
-  //   backgroundColor: 'white',
-  //   paddingBottom: 5,
-  //   paddingLeft: 24,
-  //   paddingRight: 24,
-  // },
-  // textPregunta: {
-  //   color: 'grey',
-  //   margin: 3,
-  // },
   container: {
     flex: 1,
     backgroundColor: '$primaryColor',
