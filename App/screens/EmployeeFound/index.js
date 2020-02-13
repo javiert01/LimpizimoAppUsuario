c248f2e2f47809beb8ee5a513259c748d4f1e351
javiert01 <javotinoco@hotmail.com>
2020-02-13 09:40:49 -0500

    added assignedEmployee in EmployeeFound screen and ImageAssignedEmployee component

--- App/screens/EmployeeFound/index.js
+++ App/screens/EmployeeFound/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Touchable from 'react-native-platform-touchable';
 import ImageAssignedEmployee from '../../components/ImageAssignedEmployee';
 import styles from './styles';
 import { strings } from '../../i18n';
+import { setIsServiceAssigned } from '../../store/actions/services';
 
 const EmployeeFound = props => {
-  
+  const dispatch = useDispatch();
   const assignedEmployee = useSelector(state => state.employee.assignedEmployee);
   
   const _navigateToServiceDetail = () => {
@@ -15,6 +16,7 @@ const EmployeeFound = props => {
       routeName: 'Home',
       key: 'Home', 
     });
+    dispatch(setIsServiceAssigned(false));
   }
 
 
@@ -22,7 +24,7 @@ const EmployeeFound = props => {
       <View style={styles.container}>
       <Text style={styles.titleText}>{strings('employeeFound.message')}</Text>
       <ImageAssignedEmployee />
-        <Text style={styles.titleText}>{assignedEmployee.nombre + ' ' + assignedEmployee.apellidos[0]}</Text>
+        <Text style={styles.titleText}>{assignedEmployee.nombre + ' ' + assignedEmployee.apellidos.split(' ')[0]}</Text>
       <Text style={styles.text}>34 {strings('employeeFound.numberServices')}</Text>
       <View style={styles.buttonContainer}>
         <Touchable style={styles.textContainer} onPress={_navigateToServiceDetail}>
